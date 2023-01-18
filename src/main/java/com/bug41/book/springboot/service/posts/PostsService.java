0ad9da182a6c119913ed36340c7c453c39fd930b
bug41 <bugkinguswit@gmail.com>
2023-01-18 21:52:46 +0900

    게시글 수정/삭제 기능 추가

--- src/main/java/com/bug41/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/bug41/book/springboot/service/posts/PostsService.java
@@ -3,12 +3,15 @@ package com.bug41.book.springboot.service.posts;
 import com.bug41.book.springboot.domain.posts.Posts;
 import com.bug41.book.springboot.domain.posts.PostsRepository;
 import com.bug41.book.springboot.web.Dto.PostsResponseDto;
+import com.bug41.book.springboot.web.Dto.PostsListResponseDto;
 import com.bug41.book.springboot.web.Dto.PostsSaveRequestDto;
 import com.bug41.book.springboot.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,4 +40,20 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
